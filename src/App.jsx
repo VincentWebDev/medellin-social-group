3524734dddaa06479ffc4cbd35c9d095cba69d22
Vincent Croos <vincent.croos@gmail.com>
2025-06-03 16:41:02 -0500

    coming soon font

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { Container, Row, Col, ProgressBar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
+import "./custom.scss";
 
 import "./App.css";
 
 function App() {
   const now = 10;
   return (
-    <Container className="d-flex h-100 justify-content-center align-items-center">
-      <Row>
+    <Container className="d-flex h-100  justify-content-center align-items-center">
+      <Row className="d-flex w-75">
         <Col>
-          <h1>Próximamente</h1>
+          <h1 className="display-1 d-flex justify-content-center">
+            Próximamente
+          </h1>
+
           <ProgressBar now={now} label={`${now}%`} visuallyHidden />
+          <p className="display-6">10%</p>
         </Col>
       </Row>
     </Container>
